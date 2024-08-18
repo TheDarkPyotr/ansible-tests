a6c2f939ca4023592d0b2120b9d92dd3f4bc7bdf
TheDarkPyotr <lucapinta@live.it>
2024-08-18 11:34:54 +0200

    boh4

--- utils/deploy_app/update_sla.py
+++ utils/deploy_app/update_sla.py
@@ -252,7 +252,6 @@ if __name__ == "__main__":
     loop = asyncio.get_event_loop()
     # Schedule main for execution using run_in_executor
     loop.run_in_executor(None, main)
-    loop.run_forever()  # Run the event loop
 
 #  sudo python3 ./utils/deploy_app/update_sla.py ./topologies/two-clusters.json  "[\"xavier1\", \"xavier2\"]" "[\"131.159.25.108\", \"pi4-base\"]" "[\"localhost\"]"
 #  sudo /bin/python3 ./utils/deploy_app/update_sla.py ./topologies/two-clusters.json  "[\"xavier2\"]" "[\"pi4-base\"]" "[\"131.159.25.107\"]"
