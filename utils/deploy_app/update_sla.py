aba9cceee9ee81a264109d9590ddd0b9855b4b01
TheDarkPyotr <lucapinta@live.it>
2024-08-18 10:06:21 +0200

    restore run asyncio debug

--- utils/deploy_app/update_sla.py
+++ utils/deploy_app/update_sla.py
@@ -241,11 +241,7 @@ async def main_async():
 
 
 def main():
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main_async())
-    loop.close()
-
-    # asyncio.run(main_async())
+    asyncio.run(main_async())
 
 
 if __name__ == "__main__":
