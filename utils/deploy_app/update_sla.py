26de260ee0751b67e7273b556256755806c76c84
TheDarkPyotr <lucapinta@live.it>
2024-08-20 14:20:19 +0200

    json load

--- utils/deploy_app/update_sla.py
+++ utils/deploy_app/update_sla.py
@@ -268,6 +268,7 @@ async def application_healthcheck(deployed_apps, worker_list, SYSTEM_MANAGER_URL
     if request_status in (200, 201):
         print(f"Request status: {request_status}")
         print(f"Request body: {request_body}")
+        request_body = json.loads(request_body)
         if request_body:
             print(f"Request body is a list of {len(request_body)} elements.")
             for service in request_body:
